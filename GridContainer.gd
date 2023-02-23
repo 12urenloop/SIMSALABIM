4da620db92ded6e7bbf9e1464cde5e815b87a3f6
mcbloch <git@mcbloch.dev>
2023-02-23 09:01:52 +0100

    Fix baton log performance, add visible toggle

--- GridContainer.gd
+++ GridContainer.gd
@@ -8,15 +8,11 @@ func _ready() -> void:
 			baton.connect("detection_registered", self, "_on_detection_registered")
 
 func _on_detection_registered(station, rssi, timestamp):
-	var label_baton = Label.new()
-	label_baton.text = selected_baton
-	add_child(label_baton)
-	var label_station = Label.new()
-	label_station.text = station.name
-	add_child(label_station)
-	var label = Label.new()
-	label.text = str(rssi)
-	add_child(label)
-	var label2 = Label.new()
-	label2.text = str(timestamp)
-	add_child(label2)
+	$NameValues.text += selected_baton
+	$NameValues.text += '\n'
+	$StationValues.text += station.name
+	$StationValues.text += '\n'
+	$RssiValues.text += str(rssi)
+	$RssiValues.text += '\n'
+	$TimestampValues.text += str(timestamp)
+	$TimestampValues.text += '\n'
---
+++ VBoxBaton.gd
@@ -0,0 +1,20 @@
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Show_toggled(button_pressed: bool) -> void:
+	$ScrollBatonLogs.visible = button_pressed
