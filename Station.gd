ba5cb45b0865d96f15da60fdca2bc6a8bda15d0c
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-04-01 17:52:36 +0200

    feat(wsServer): add a basic websocket server on ports 5000-5006

--- Station.gd
+++ Station.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var port: int
+@onready var _ws_server: WebSocketServer
 
 var detections = []
 var last_id: int = 0
@@ -13,6 +14,14 @@ var hover: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$UI/Name.text = name
+	_ws_server = WebSocketServer.new()
+	var err = _ws_server.listen(port-1000)
+	if err != OK:
+		print("Error listing on port %s" % (port-1000))
+		return
+	_ws_server.connect("message_received", _on_web_socket_server_message_received)
+	_ws_server.connect("client_connected", _on_web_socket_server_client_connected)
+	_ws_server.connect("client_disconnected", _on_web_socket_server_client_disconnected)
 
 func _draw() -> void:
 	# draw_set_transform(Vector2(18,64),0,Vector2(2,3))
@@ -42,9 +51,30 @@ func add_detection(baton: PathFollow2D):
 	
 	# $DetectionGraph.add_point(timestamp, new_rssi*-1)
 	baton.emit_signal("detection_registered", self, new_rssi, timestamp)
+	if _ws_server.tcp_server.is_listening():
+		_ws_server.send(0, websocket_response(last_id-1))
+
+
+func _on_web_socket_server_client_connected(peer_id):
+	var peer: WebSocketPeer = _ws_server.peers[peer_id]
+	print("Remote client connected: %d. Protocol: %s" % [peer_id, peer.get_selected_protocol()])
+
+func _on_web_socket_server_client_disconnected(peer_id):
+	var peer: WebSocketPeer = _ws_server.peers[peer_id]
+	print("Remote client disconnected: %d. Code: %d, Reason: %s" % [peer_id, peer.get_close_code(), peer.get_close_reason()])
+
+func _on_web_socket_server_message_received(peer_id, message):
+	print("Server received data from peer %d: %s" % [peer_id, message])
+	var parsed_message = JSON.parse_string(message)
+	if parsed_message == null:
+		print("Received an invalid websocket message")
+	_ws_server.send(peer_id, websocket_response(parsed_message.lastId))
+	var lastId = parsed_message.lastId
 
 func _process(delta):
 	queue_redraw()
+	
+	_ws_server.poll()
 	
 	# TODO
 	# Check if a baton is in the area of the station. Add a detection with a realistic rssi
@@ -60,6 +90,11 @@ func response(index):
 		"detections": detections.slice(index, detections.size())
 	})
 
+func websocket_response(index):
+	return JSON.stringify([{
+		"station_id": self.name,
+		"detections": detections.slice(index, detections.size())
+	}])
 
 func _on_CheckButton_toggled(button_pressed: bool) -> void:
 	enabled = button_pressed
