fdf279b0d65f0da2630e1820d07eaf3668cda673
mcbloch <git@mcbloch.dev>
2026-04-28 20:39:10 +0200

    update easy_charts addon

--- addons/easy_charts/control_charts/chart.gd
+++ addons/easy_charts/control_charts/chart.gd
@@ -1,24 +1,32 @@
+@icon("res://addons/easy_charts/utilities/icons/linechart.svg")
 extends PanelContainer
-class_name Chart, "res://addons/easy_charts/utilities/icons/linechart.svg"
+class_name Chart
 
-onready var _canvas: Canvas = $Canvas
-onready var plot_box: PlotBox = $"%PlotBox"
-onready var grid_box: GridBox = $"%GridBox"
-onready var functions_box: Control = $"%FunctionsBox"
-onready var function_legend: FunctionLegend = $"%FunctionLegend"
+@onready var _canvas: Canvas = $Canvas
+@onready var plot_box: PlotBox = $"%PlotBox"
+@onready var grid_box: GridBox = $"%GridBox"
+@onready var functions_box: Control = $"%FunctionsBox"
+@onready var function_legend: FunctionLegend = $"%FunctionLegend"
 
 var functions: Array = []
 var x: Array = []
 var y: Array = []
 
-var x_labels: PoolStringArray = []
-var y_labels: PoolStringArray = []
+var x_labels_function: Callable = Callable()
+var y_labels_function: Callable = Callable()
 
-var chart_properties: ChartProperties = ChartProperties.new()
+var x_domain: Dictionary = {}
+var y_domain: Dictionary = {}
+
+var chart_properties: ChartProperties = null
 
 ###########
 
-func plot(functions: Array, properties: ChartProperties = ChartProperties.new()) -> void:
+func _ready() -> void:
+	if theme == null:
+		theme = Theme.new()
+
+func plot(functions: Array[Function], properties: ChartProperties = ChartProperties.new()) -> void:
 	self.functions = functions
 	self.chart_properties = properties
 	
@@ -32,8 +40,6 @@ func plot(functions: Array, properties: ChartProperties = ChartProperties.new())
 func get_function_plotter(function: Function) -> FunctionPlotter:
 	var plotter: FunctionPlotter
 	match function.get_type():
-		Function.Type.SCATTER:
-			plotter = ScatterPlotter.new(function)
 		Function.Type.LINE:
 			plotter = LinePlotter.new(function)
 		Function.Type.AREA:
@@ -42,67 +48,112 @@ func get_function_plotter(function: Function) -> FunctionPlotter:
 			plotter = PiePlotter.new(function)
 		Function.Type.BAR:
 			plotter = BarPlotter.new(function)
+		Function.Type.SCATTER, _:
+			plotter = ScatterPlotter.new(function)
 	return plotter
 
-func load_functions(functions: Array) -> void:
+func load_functions(functions: Array[Function]) -> void:
 	self.x = []
 	self.y = []
 	
 	function_legend.clear()
+
+	# Remove existing function_plotters
+	for function_plotter in functions_box.get_children():
+		functions_box.remove_child(function_plotter)
+		function_plotter.queue_free()
 	
 	for function in functions:
 		# Load x and y values
-		self.x.append(function.x)
-		self.y.append(function.y)
-		
-		# Load Labels
-		if self.x_labels.empty():
-			if ECUtilities._contains_string(function.x):
-				self.x_labels = function.x
+		self.x.append(function.__x)
+		self.y.append(function.__y)
 		
 		# Create FunctionPlotter
 		var function_plotter: FunctionPlotter = get_function_plotter(function)
-		function_plotter.connect("point_entered", plot_box, "_on_point_entered")
-		function_plotter.connect("point_exited", plot_box, "_on_point_exited")
+		function_plotter.connect("point_entered", Callable(plot_box, "_on_point_entered"))
+		function_plotter.connect("point_exited", Callable(plot_box, "_on_point_exited"))
 		functions_box.add_child(function_plotter)
 		
 		# Create legend
 		match function.get_type():
 			Function.Type.PIE:
-				for i in function.x.size():
-					var interp_color: Color = function.get_gradient().interpolate(float(i) / float(function.x.size()))
-					function_legend.add_label(function.get_type(), interp_color, Function.Marker.NONE, function.y[i])
+				for i in function.__x.size():
+					var interp_color: Color = function.get_gradient().sample(float(i) / float(function.__x.size()))
+					function_legend.add_label(function.get_type(), interp_color, Function.Marker.NONE, function.__y[i])
 			_:
 				function_legend.add_function(function)
 
 func _draw() -> void:
-	# GridBox
-	var x_domain: Dictionary = calculate_domain(x)
-	var y_domain: Dictionary = calculate_domain(y)
+	if (x.size() == 0) or (y.size() == 0) or (x.size() == 1 and x[0].is_empty()) or (y.size() == 1 and y[0].is_empty()):
+		printerr("Cannot plot an empty function!")
+		return
 	
-	var plotbox_margins: Vector2 = calculate_plotbox_margins(x_domain, y_domain)
+	var is_x_fixed: bool = x_domain.get("fixed", false)
+	var is_y_fixed: bool = y_domain.get("fixed", false)
+	
+	# GridBox
+	if not is_x_fixed or not is_y_fixed :
+		if chart_properties.max_samples > 0 :
+			var _x: Array = []
+			var _y: Array = []
+			
+			_x.resize(x.size())
+			_y.resize(y.size())
+			
+			for i in x.size():
+				if not is_x_fixed:
+					_x[i] = x[i].slice(max(0, x[i].size() - chart_properties.max_samples), x[i].size())
+				if not is_y_fixed:
+					_y[i] = y[i].slice(max(0, y[i].size() - chart_properties.max_samples), y[i].size())
+			
+			if not is_x_fixed:
+				x_domain = calculate_domain(_x)
+			if not is_y_fixed:
+				y_domain = calculate_domain(_y)
+		else:
+			if not is_x_fixed:
+				x_domain = calculate_domain(x)
+			if not is_y_fixed:
+				y_domain = calculate_domain(y)
 	
 	# Update values for the PlotBox in order to propagate them to the children
-	plot_box.box_margins = plotbox_margins
+	update_plotbox(x_domain, y_domain, x_labels_function, y_labels_function)
 	
 	# Update GridBox
-	update_gridbox(x_domain, y_domain, self.x_labels, self.y_labels)
+	update_gridbox(x_domain, y_domain, x_labels_function, y_labels_function)
 	
 	# Update each FunctionPlotter in FunctionsBox
 	for function_plotter in functions_box.get_children():
-		function_plotter.update_values(x_domain, y_domain)
+		if function_plotter is FunctionPlotter:
+			function_plotter.visible = function_plotter.function.get_visibility()
+			if function_plotter.function.get_visibility():
+				function_plotter.update_values(x_domain, y_domain)
 
 func calculate_domain(values: Array) -> Dictionary:
 	for value_array in values:
 		if ECUtilities._contains_string(value_array):
-			return { lb = 0.0, ub = (value_array.size() - 1), has_decimals = false }
+			return { lb = 0.0, ub = (value_array.size() - 1), has_decimals = false , fixed = false }
 	var min_max: Dictionary = ECUtilities._find_min_max(values)
-	return { lb = ECUtilities._round_min(min_max.min), ub = ECUtilities._round_max(min_max.max), has_decimals = ECUtilities._has_decimals(values) }
+	
+	if not chart_properties.smooth_domain:
+		return { lb = min_max.min, ub = min_max.max, has_decimals = ECUtilities._has_decimals(values), fixed = false }
+	else:
+		return { lb = ECUtilities._round_min(min_max.min), ub = ECUtilities._round_max(min_max.max), has_decimals = ECUtilities._has_decimals(values) , fixed = false }
 
-func update_gridbox(x_domain: Dictionary, y_domain: Dictionary, x_labels: PoolStringArray, y_labels: PoolStringArray) -> void:
+func set_x_domain(lb: Variant, ub: Variant) -> void:
+	x_domain = { lb = lb, ub = ub, has_decimals = ECUtilities._has_decimals([lb, ub]), fixed = true }
+
+func set_y_domain(lb: Variant, ub: Variant) -> void:
+	y_domain = { lb = lb, ub = ub, has_decimals = ECUtilities._has_decimals([lb, ub]), fixed = true }
+
+func update_plotbox(x_domain: Dictionary, y_domain: Dictionary, x_labels_function: Callable, y_labels_function: Callable) -> void:
+	plot_box.box_margins = calculate_plotbox_margins(x_domain, y_domain)
+	plot_box.set_labels_functions(x_labels_function, y_labels_function)
+
+func update_gridbox(x_domain: Dictionary, y_domain: Dictionary, x_labels_function: Callable, y_labels_function: Callable) -> void:
 	grid_box.set_domains(x_domain, y_domain)
-	grid_box.set_labels(x_labels, y_labels)
-	grid_box.update()
+	grid_box.set_labels_functions(x_labels_function, y_labels_function)
+	grid_box.queue_redraw()
 
 func calculate_plotbox_margins(x_domain: Dictionary, y_domain: Dictionary) -> Vector2:
 	var plotbox_margins: Vector2 = Vector2(
@@ -118,13 +169,13 @@ func calculate_plotbox_margins(x_domain: Dictionary, y_domain: Dictionary) -> Ve
 		if y_domain.lb < 0: # negative number
 			var y_min_formatted: String = ECUtilities._format_value(y_domain.lb, y_domain.has_decimals)
 			if y_min_formatted.length() >= y_max_formatted.length():
-				 y_ticklabel_size = chart_properties.font.get_string_size(y_min_formatted)
+				y_ticklabel_size = chart_properties.get_string_size(y_min_formatted)
 			else:
-				y_ticklabel_size = chart_properties.font.get_string_size(y_max_formatted)
+				y_ticklabel_size = chart_properties.get_string_size(y_max_formatted)
 		else:
-			y_ticklabel_size = chart_properties.font.get_string_size(y_max_formatted)
+			y_ticklabel_size = chart_properties.get_string_size(y_max_formatted)
 		
 		plotbox_margins.x += y_ticklabel_size.x + chart_properties.x_ticklabel_space
-		plotbox_margins.y += chart_properties.font.size + chart_properties.y_ticklabel_space
+		plotbox_margins.y += ThemeDB.fallback_font_size + chart_properties.y_ticklabel_space
 	
 	return plotbox_margins
--- addons/easy_charts/control_charts/plotters/bar_plotter.gd
+++ addons/easy_charts/control_charts/plotters/bar_plotter.gd
@@ -5,16 +5,18 @@ class_name BarPlotter
 signal point_entered(point, function)
 signal point_exited(point, function)
 
-var bars: PoolVector2Array
+var bars: PackedVector2Array
 var bars_rects: Array
 var focused_bar_midpoint: Point
 
 var bar_size: float
 
-func _init(function: Function).(function) -> void:
+func _init(function: Function) -> void:
+	super(function)
 	self.bar_size = function.props.get("bar_size", 5.0)
 
 func _draw() -> void:
+	super._draw()
 	var box: Rect2 = get_box()
 	var x_sampled_domain: Dictionary = { lb = box.position.x, ub = box.end.x }
 	var y_sampled_domain: Dictionary = { lb = box.end.y, ub = box.position.y }
@@ -24,10 +26,10 @@ func _draw() -> void:
 func sample(x_sampled_domain: Dictionary, y_sampled_domain: Dictionary) -> void:
 	bars = []
 	bars_rects = []
-	for i in function.x.size():
+	for i in function.__x.size():
 		var top: Vector2 = Vector2(
 			ECUtilities._map_domain(i, x_domain, x_sampled_domain),
-			ECUtilities._map_domain(function.y[i], y_domain, y_sampled_domain)
+			ECUtilities._map_domain(function.__y[i], y_domain, y_sampled_domain)
 		)
 		var base: Vector2 = Vector2(top.x, ECUtilities._map_domain(0.0, y_domain, y_sampled_domain))
 		bars.push_back(top)
@@ -42,7 +44,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouse:
 		for i in bars_rects.size():
 			if bars_rects[i].grow(5).abs().has_point(get_relative_position(event.position)):
-				var point: Point = Point.new(bars_rects[i].get_center(), { x = function.x[i], y = function.y[i]})
+				var point: Point = Point.new(bars_rects[i].get_center(), { x = function.__x[i], y = function.__y[i]})
 				if focused_bar_midpoint == point:
 					return
 				else:
